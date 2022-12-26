026ddd4f11ff22803a43ae6d46418ccbbcb1cf14
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-27 06:55:46 +0800

    add content form

---
+++ src/components/contents/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import '/src/assets/css/contents/home.css'
+</script>
+
+<template>
+    <div class="content__text">
+        <h1 class="text__title">Full Stack Web Developer</h1>
+        <h3 class="text__description">a full stack web developer</h3>
+    </div>
+
+    <form action="#" class="content__form">
+        <input type="email" name="email" id="email" class="input-email form__email" placeholder="Email adress..">
+        <button type="submit" class="button form__submit">Connect With Me</button>
+    </form>
+</template>
