261ec6d3d5ec88f2a507ea1296453e8da215f1fb
ghazwulshaf <ghazwul.s@gmail.com>
2022-12-27 09:29:55 +0800

    add content image

--- src/components/contents/Home.vue
+++ src/components/contents/Home.vue
@@ -3,13 +3,15 @@ import '/src/assets/css/contents/home.css'
 </script>
 
 <template>
-    <div class="content__text">
-        <h1 class="text__title">Full Stack Web Developer</h1>
-        <h3 class="text__description">a full stack web developer</h3>
-    </div>
-
-    <form action="#" class="content__form">
-        <input type="email" name="email" id="email" class="input-email form__email" placeholder="Email address..">
-        <button type="submit" class="button form__submit">Connect With Me</button>
-    </form>
+    <section class="content">
+        <div class="content__text">
+            <h1 class="text__title">Full Stack Web Developer</h1>
+            <h3 class="text__description">a full stack web developer</h3>
+        </div>
+    
+        <form action="#" class="content__form">
+            <input type="email" name="email" id="email" class="input-email form__email" placeholder="Email Address..">
+            <button type="submit" class="button form__submit">Connect With Me</button>
+        </form>
+    </section>
 </template>
